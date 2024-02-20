def6c5f1a2d9a062d6032669f5195fe74b3e9696
ScriptMaster <scriptmaster@gmail.com>
2024-02-21 03:32:36 +0530

    sergeant create dashboard mydashboard

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export function Header() {
     return <header>
         <nav>
-            <a href="/">Company Dashboard</a>
+            <a href="/">Company + Logo</a>
             <ul>
                 <li><a href="/">Dashboard</a></li>
                 <li><a href="/files">Files</a></li>
