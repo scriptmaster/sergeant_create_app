c8ec5a70d89746729c263ad02eae0045274533f5
ScriptMaster <scriptmaster@gmail.com>
2024-02-20 23:53:52 +0530

    sir create react myapp

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sergeant_create_app
+
+```
+sir create react myapp
+```
+
+This will create under services/myapp (similar to apps/myapp)
+
+then do:
+
+```
+sir --serve
+```
+
+to build and serve the project
---
+++ main.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<h1>Welcome to sergeant React app</h1>);
