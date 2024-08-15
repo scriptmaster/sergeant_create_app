33865ab8ab849e2bdd8ac6ec804c069a4305a26a
ScriptMaster <scriptmasterai@gmail.com>
2024-08-16 01:05:33 +0530

    routes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,25 @@ import * as React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Header } from "./components/header.tsx";
-import { routes } from "./routes.tsx";
+import Home from "./pages/home/home.tsx";
+import Files from "./pages/files/files.tsx";
+import Settings from "./pages/settings/settings.tsx";
+import Account from "./pages/account/account.tsx";
 
 export default function App() {
-    const router = createBrowserRouter(routes);
+    const router = createBrowserRouter([{
+        path: "/",
+        element: <Home />
+    }, {
+        path: "/files",
+        element: <Files />,
+    }, {
+        path: "/settings",
+        element: <Settings />,
+    }, {
+        path: "/account",
+        element: <Account />,
+    }]);
 
     return <>
         <Header />
--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
-import "https://unpkg.com/emeraldcss@1.0.1/dist/cdn/main.css";
+import "emeraldcss";
 import "./main.scss";
 
 import App from "./App.tsx";
