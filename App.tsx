de5e1a671cf4ed1ff2022016170a114bce791b51
ScriptMaster <scriptmasterai@gmail.com>
2024-08-16 01:09:47 +0530

    Added routes.tsx

--- App.tsx
+++ App.tsx
@@ -2,25 +2,10 @@ import * as React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Header } from "./components/header.tsx";
-import Home from "./pages/home/home.tsx";
-import Files from "./pages/files/files.tsx";
-import Settings from "./pages/settings/settings.tsx";
-import Account from "./pages/account/account.tsx";
+import { routes } from "./routes.tsx";
 
 export default function App() {
-    const router = createBrowserRouter([{
-        path: "/",
-        element: <Home />
-    }, {
-        path: "/files",
-        element: <Files />,
-    }, {
-        path: "/settings",
-        element: <Settings />,
-    }, {
-        path: "/account",
-        element: <Account />,
-    }]);
+    const router = createBrowserRouter(routes);
 
     return <>
         <Header />
